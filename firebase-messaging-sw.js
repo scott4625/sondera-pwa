009aedbf5e4bed6ad121959ccb4976b9f51202d0
scott4625 <scott@kristenboss.com>
2025-07-27 10:49:34 -0600

    Add files via upload

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,4 +1,3 @@
-
 importScripts('https://www.gstatic.com/firebasejs/12.0.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/12.0.0/firebase-messaging-compat.js');
 
@@ -6,7 +5,7 @@ firebase.initializeApp({
   apiKey: "AIzaSyDzjbq339_QizKUVygfbX2WjQ5Mb_4SLys",
   authDomain: "sondera-pwa.firebaseapp.com",
   projectId: "sondera-pwa",
-  storageBucket: "sondera-pwa.firebasestorage.app",
+  storageBucket: "sondera-pwa.appspot.com",
   messagingSenderId: "1006942535696",
   appId: "1:1006942535696:web:f136db480044a602e30a95",
   measurementId: "G-NXX9DYK611"
--- messaging-init.js
+++ messaging-init.js
@@ -1,4 +1,3 @@
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
 import { getMessaging, getToken, onMessage } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-messaging.js";
 
@@ -6,30 +5,29 @@ const firebaseConfig = {
   apiKey: "AIzaSyDzjbq339_QizKUVygfbX2WjQ5Mb_4SLys",
   authDomain: "sondera-pwa.firebaseapp.com",
   projectId: "sondera-pwa",
-  storageBucket: "sondera-pwa.firebasestorage.app",
+  storageBucket: "sondera-pwa.appspot.com",
   messagingSenderId: "1006942535696",
   appId: "1:1006942535696:web:f136db480044a602e30a95",
   measurementId: "G-NXX9DYK611"
 };
 
-const vapidKey = "BLOAk7Zxjljd293LeS-KStSw9RSfFSTlOwusah4KebhfYSV4cD6LIIeLN8VTxySB_OHRW0Wjzrjin97hIr5_KlM";
-
 const app = initializeApp(firebaseConfig);
 const messaging = getMessaging(app);
 
-Notification.requestPermission().then((permission) => {
-  if (permission === 'granted') {
-    return getToken(messaging, { vapidKey });
-  } else {
-    console.warn("Permission not granted for notifications");
+document.getElementById("notifyBtn").addEventListener("click", async () => {
+  try {
+    const permission = await Notification.requestPermission();
+    if (permission === 'granted') {
+      const token = await getToken(messaging, {
+        vapidKey: "BLOAk7Zxjljd293LeS-KStSw9RSfFSTlOwusah4KebhfYSV4cD6LIIeLN8VTxySB_OHRW0Wjzrjin97hIr5_KlM"
+      });
+      document.getElementById("status").innerText = "Notifications enabled!";
+      console.log("Token:", token);
+    } else {
+      document.getElementById("status").innerText = "Permission denied.";
+    }
+  } catch (err) {
+    console.error(err);
+    document.getElementById("status").innerText = "Error requesting permission.";
   }
-}).then((token) => {
-  if (token) {
-    console.log("Push token:", token);
-    // You can send this to your server
-  }
-});
-
-onMessage(messaging, (payload) => {
-  console.log("Message received in foreground:", payload);
 });
